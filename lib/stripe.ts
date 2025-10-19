9369dd71cb4f6da77324e0f51b6793e0bec86aba
KomarovD <132566683+komarov-dc@users.noreply.github.com>
2025-10-19 21:43:32 +0300

    Fix: Use correct Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2025-06-30",
+  apiVersion: "2025-09-30.clover",
   typescript: true,
 });
 
